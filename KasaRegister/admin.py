22d076b6e123073d08492a1d9a2dd73fa3f4d4f0
JohnniePM <johnnie.jpm@outlook.com>
2023-05-11 16:54:19 +0200

    last

--- KasaRegister/admin.py
+++ KasaRegister/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 
-from kasa.models import user,IdFiles
-# Register your models here.
-admin.site.register(user)
-admin.site.register(IdFiles)
 
 
--- KasaRegister/apps.py
+++ KasaRegister/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class KasaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kasa'
+    name = 'KasaRegister'
